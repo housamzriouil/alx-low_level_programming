5c1829f90eb9d455667fb2fd3c401c5a40aaa3b2
myusername <myemail@gmail.com>
2023-03-16 19:37:52 -0700

    UNIX is basically a simple operating system

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
  */
 int main(void)
 {
-	fput(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	fprintf(stdout, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
