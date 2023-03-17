f471d0b6e43ab7cf308bd17c2e41373ed68138c0
myusername <myemail@gmail.com>
2023-03-17 15:57:45 -0700

    When I was having that alphabet soup

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-        char C;
+	char low, e, q;
 
-        do {
-		for (C = 'a' ; C <= 'z' ; C++);
-                putchar(C);
-        } while (C!='e' || C!='q'); 
-                putchar('\n');
+	e = 'e';
+	q = 'q';
 
-        return (0);
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		if (low != e && low != q)
+			putchar(low);
+	}
+	putchar('\n');
+
+	return (0);
 }
