e764c771f4ae28abce0750811a9fa74ef08d3f69
myusername <myemail@gmail.com>
2023-03-17 15:39:20 -0700

    When I was having that alphabet soup

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,11 +10,9 @@ int main(void)
         char C;
 
         do {
-		{
-			for (C = 'a' ; C <= 'z' ; C++)
-		}
+		for (C = 'a' ; C <= 'z' ; C++);
                 putchar(C);
-        } while (C!='e' || C!='q') 
+        } while (C!='e' || C!='q'); 
                 putchar('\n');
 
         return (0);
