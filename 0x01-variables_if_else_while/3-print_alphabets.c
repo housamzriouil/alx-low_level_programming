495443666d4781d878f3fd021d35558e196a31f8
myusername <myemail@gmail.com>
2023-03-17 14:17:57 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char C;
+
+        for (C = 'a' ; C <= 'Z' ; C++)
+        {
+                putchar(C);
+        }
+                putchar('\n');
+
+        return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,1 +1,1 @@
-1. The last digit
+3. alphABET
