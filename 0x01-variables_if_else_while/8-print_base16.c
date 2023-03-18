61d665f0e9164e2784434e2689d0cd760c7befd6
myusername <myemail@gmail.com>
2023-03-17 18:02:03 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char n, C;
+
+	for (n = 0 ; n < 10 ; n++)
+                putchar('0' + n);
+        for (C= 'A'; C <= 'Z'; C++)
+		putchar(C);
+	putchar('\n');
+
+	return (0);
+}
