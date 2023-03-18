0d4d278d2e07cebce8dd8faac070f7f787173f56
myusername <myemail@gmail.com>
2023-03-17 18:38:03 -0700

    Inventing is a combination of brains

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,19 +10,23 @@
 
 int main(void)
 {
-	int d;
+	int digit1, digit2;
 
-	for (d = 0; d < 90; d++)
+	for (digit1 = 0; digit1 < 10; digit1++)
 	{
-		putchar((d / 10) + '1');
-		putchar((d % 10) + '0');
-	
-		if (d != 89)
+		for (digit2 = 0; digit2 < 10; digit2++)
 		{
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
+
+			if (digit1 == 9 && digit2 == 9)
+				continue;
+
 			putchar(',');
 			putchar(' ');
 		}
 	}
+
 	putchar('\n');
 
 	return (0);
