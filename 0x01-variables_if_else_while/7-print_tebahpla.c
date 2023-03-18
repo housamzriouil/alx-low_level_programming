c208795653fff26105819d5bec6bbe38a1595540
myusername <myemail@gmail.com>
2023-03-17 17:12:49 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char C;
+
+	for (C = 'z'; C <= 'a'; C--)
+		putchar(C);
+		putchar('\n');
+
+	return (0);
+}
