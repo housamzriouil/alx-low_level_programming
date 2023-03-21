c4bbcd0f08e21ef3a724b5592511c07309a85ff6
myusername <myemail@gmail.com>
2023-03-21 16:01:04 -0700

    There is no such thing

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
  * _abs - a function that computes the absolute value of an integer
- * @ab: integer input
- * Return: absolute value of ab
+ * @n: integer input
+ * Return: absolute value of n
  */
 int _abs(int n)
 {
-        if (n > 0)
-        {
+	if (n > 0)
+	{
 		return (n);
-        } else if (n == 0)
-        {
+	} else if (n == 0)
+	{
 		return (0);
-        }
-        {
+	}
+	{
 		return (-n);
-        }
+	}
 }
