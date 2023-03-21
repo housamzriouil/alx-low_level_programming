00b7e1d2cb0bbf87b152f8c98970799fd0fadc73
myusername <myemail@gmail.com>
2023-03-21 15:07:21 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,5 +1,6 @@
 #include "main.h"
 /**
+ * * @n: number input
  * print_sign - a function that prints the sign of a number
  * Return: 1 if n is greater than zero, 
  * Returns 0 if n is zero,
@@ -11,12 +12,12 @@ int print_sign(int n)
 	{ 
 		_putchar('+');
 		return (1);
-	}else if (n == 0) 
-	{ 
+	} else if (n == 0) 
+	{
 		_putchar('0');
 		return (0);
-	}else 
-	{ 
+	} else 
+	{
 		_putchar('-');
 		return (-1);
 	}
