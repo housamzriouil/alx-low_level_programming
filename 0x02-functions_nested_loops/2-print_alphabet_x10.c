16732abbff73686f5c909bea4ef4f27797553f04
myusername <myemail@gmail.com>
2023-03-21 12:47:58 -0700

    alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,10 +8,10 @@ void print_alphabet_x10(void)
 	int i;
 	char C;
 
-	for (i = 0 ; i < 10 ; i++ )
+	for (i = 0 ; i < 10 ; i++)
+		_putchar('\n');
 	   for (C = 'a' ; C <= 'z' ; C++)
 	   {
                 _putchar(C);
 	   }
-                _putchar('\n');
 }
