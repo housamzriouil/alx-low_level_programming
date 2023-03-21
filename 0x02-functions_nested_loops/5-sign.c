d1ddde01473302d98894f8bdec34184be783c999
myusername <myemail@gmail.com>
2023-03-21 15:14:45 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * * @n: number input
+ * @n: number input
  * print_sign - a function that prints the sign of a number
- * Return: 1 if n is greater than zero, 
+ * Return 1 if n is greater than zero, 
  * Returns 0 if n is zero,
  * Returns -1 if n is less than zero
  */
@@ -12,11 +12,11 @@ int print_sign(int n)
 	{ 
 		_putchar('+');
 		return (1);
-	} else if (n == 0) 
+	} else if (n == 0)
 	{
 		_putchar('0');
 		return (0);
-	} else 
+	}
 	{
 		_putchar('-');
 		return (-1);
