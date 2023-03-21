f4327e6d63b49784554bd5c4da04174ce24dbbfd
myusername <myemail@gmail.com>
2023-03-21 09:02:23 -0700

    I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void);
+{
+	char C;
+
+	for (C = 'a' ; C <= 'z' ; C++)
+	{
+		putchar(C);
+	}
+		putchar('\n');
+		return (0);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-0. _putchar
+1. I sometimes suffer from insomnia
