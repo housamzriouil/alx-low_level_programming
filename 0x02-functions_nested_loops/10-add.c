afea5d67d6c5c808e2acc5a8f4fa0d8eec312a4d
myusername <myemail@gmail.com>
2023-03-21 18:39:29 -0700

    a + b

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,7 +3,6 @@
  * add - a function that adds two integers and returns the result
  * @a: first number input
  * @b: second number input
- * @n: result of a + b
  * Return: the value of a + b
  */
 int add(int a, int b)
