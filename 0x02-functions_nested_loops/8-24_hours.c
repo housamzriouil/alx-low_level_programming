e48850e76674a0d061f457e4861c1de896c0b1c6
myusername <myemail@gmail.com>
2023-03-21 17:17:04 -0700

    There are only 3 colors

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints lowercase alphabet_x10
+ * Return: 0
+ */
+void jack_bauer(void);
+{
+	int i;
+	int j;
+
+	for (i = 00 ; i < 60 ; i++)
+	{
+		for (j = 0 ; j < 24 ; j++)
+		{
+			_putchar(i + '0': j + '0');
+		}
+	_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-7. There are only 3 colors, 10 digits, and 7 notes
+8. I'm federal agent Jack Bauer
