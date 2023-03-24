481ea38111a5a9135fdd711bdc7ea837cbb3a357
myusername <myemail@gmail.com>
2023-03-23 17:39:32 -0700

    I believe in numbers and signs

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,7 @@
 /**
  * print_most_numbers - a function that prints the numbers, from 0 to 9
  *
- * Return: 0-9, excluding 2 and 4, followed by new line
+ * Return: 0
  */
 void print_most_numbers(void)
 {
