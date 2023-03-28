bce29c52a9a8ff59ff08d54b98ba91dcbab053a4
myusername <myemail@gmail.com>
2023-03-27 17:49:31 -0700

    Winning is only half of it

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -24,8 +24,9 @@ void puts_half(char *str)
 	
 	len = _strlen(str);
 	mid = len / 2;
-	for (i = mid ,k = 0 ; i <= len ; i++ , k++)
+	for (i = mid ,k = 0 ; i <= len; i++ , k++)
 	{
 		_putchar(str[i]);
 	}
+	_putchar('\n');
 }
