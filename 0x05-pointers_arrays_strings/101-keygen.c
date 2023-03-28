55e1cad0025720deb26cf8eb94dedd5ff3eb025a
myusername <myemail@gmail.com>
2023-03-27 19:45:56 -0700

    hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,68 +1,24 @@
-#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <time.h>
-
-
-void randomPasswordGeneration(int N)
-{
-	
-	int i = 0;
-
-	int randomizer = 0;
-
-	
-	srand((unsigned int)(time(NULL)));
-
-	char numbers[] = "0123456789";
-
-	
-	char letter[] = "abcdefghijklmnoqprstuvwyzx";
-
-	
-	char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
-
-	char symbols[] = "!@#$^&*?";
-
-	char password[N];
-
-	
-	randomizer = rand() % 4;
-
-
-	for (i = 0; i < N; i++) {
-
-		if (randomizer == 1) {
-			password[i] = numbers[rand() % 10];
-			randomizer = rand() % 4;
-			printf("%c", password[i]);
-		}
-		else if (randomizer == 2) {
-			password[i] = symbols[rand() % 8];
-			randomizer = rand() % 4;
-			printf("%c", password[i]);
-		}
-		else if (randomizer == 3) {
-			password[i] = LETTER[rand() % 26];
-			randomizer = rand() % 4;
-			printf("%c", password[i]);
-		}
-		else {
-			password[i] = letter[rand() % 26];
-			randomizer = rand() % 4;
-			printf("%c", password[i]);
-		}
-	}
-}
 
 int main()
 {
-	
+    srand((unsigned int)(time(NULL)));
+    int i;
+    char pass[12];
 
-	int N = 10;
+    printf("Press enter to get a twelve-character password\n");
+    getchar();
 
-	randomPasswordGeneration(N);
-
-	return 0;
+    for (i = 0; i < 4; i++) {
+        pass[i] = rand() % 9;
+        char capLetter = 'A' + (rand() % 26);
+        pass[i + 2] = capLetter;
+        char letter = 'a' + (rand() % 26);
+        pass[i + 3] = letter;
+        printf("%d%c%c", pass[i], pass[i + 2], pass[i + 3]);
+    }
+    printf("\n\n");
 }
-
