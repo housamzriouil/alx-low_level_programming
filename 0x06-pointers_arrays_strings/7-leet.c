8d57a1ff1fa959cafcf77a6e466cd069e714ca03
myusername <myemail@gmail.com>
2023-03-30 16:40:10 -0700

    Mozart composed

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -5,7 +5,7 @@
  * @s: string to manipulate
  * Return: string
  */
-char *leet(char *)
+char *leet(char *s)
 {
 	int a[11] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
 	int b[11] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
