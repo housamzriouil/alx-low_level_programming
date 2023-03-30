a08fbec74dec17791db1c655e6ecd8e88c7a7563
myusername <myemail@gmail.com>
2023-03-29 19:24:52 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenate two strings
+ * @dest: string to be appended to
+ * @src: string to append
+ * Return: concatenated string
+ */
+char *_strcat(char *dest, char *src)
+{
+	_strcat(dest ,src);
+}
