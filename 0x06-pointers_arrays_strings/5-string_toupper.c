ac34d00526893da51a52f2099aef5e28a6ed3bfb
myusername <myemail@gmail.com>
2023-03-30 15:29:18 -0700

    Always look up

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,18 +1,20 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
  * string_toupper - capitalize all letters in string
  * @s: string to manipulate
  * Return: string with all letters capitalized
  */
-char *string_toupper(char *)
-{
-	int i;
 
-	for (i = 0 ; s[i] ; i++)
+char *string_toupper(char *s)
+{
+
+	int i = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] >= 97 && s[i] <= 122)
-			s[i] = s[i] - 32;
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 'a' + 'A';
 	}
 	return (s);
 }
