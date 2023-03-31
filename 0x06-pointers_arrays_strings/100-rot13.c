9c214767ce1953c6782dea0d262cc12ede98377f
myusername <myemail@gmail.com>
2023-03-30 17:27:25 -0700

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -19,17 +19,16 @@ char *rot13(char *s)
 		     'l', 'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
 		     'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
 		     'J', 'K', 'L', 'M'};
-
 	int i, j;
 
 	for (j = 0; s[j] != '\0'; j++)
 	{
-		for (i = 0; a[i] != '\0'; i++)
-		{
-			if ((s[j] == a[i]) && (s[j] != b[i]))
-				s[j] = b[i];
-		} else
-		       s[j] = a[i];
+		i = 0;
+		while (a[i] != '\0' && s[j] != a[i])
+			i++;
+
+		if (s[j] == a[i])
+			s[j] = b[i];
 	}
 
 	return (s);
