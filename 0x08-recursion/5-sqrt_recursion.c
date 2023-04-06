88b3a9e785d98af7fb2880c542564d07bb7196d8
myusername <myemail@gmail.com>
2023-04-05 19:38:53 -0700

    Your subconscious

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -5,13 +5,27 @@
  * @n: the number
  * Return: square root
  */
+int find_root(int n, int root)
+{
+	if (root * root > n)
+		return (-1);
+
+	if (root * root == n)
+		return (root);
+
+	return (find_root(n, root + 1));
+}
+
+/**
+ * _sqrt_recursion - natural square root of a number
+ * @n: the number 
+ * Return: square root
+ */
+
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	if (n == 0)
-		return (0);
-	if (n == 1)
-		return (1);
-	return (_sqrt_recursion(n + 1));
+
+	return (find_root(n, 0));
 }
