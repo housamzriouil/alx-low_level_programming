9ded0fc4999ccf76dab66cc1d81aee5bcbb41da7
myusername <myemail@gmail.com>
2023-04-13 05:08:22 -0700

    malloc checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main"
+#include <stdlib.h>
+
+/**
+ * *malloc_checked - allocates memory using malloc
+ * @b: size to malloc
+ * Return: pointer to allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int* ptr;
+	int n, i;
+
+	ptr = (int*)malloc(n * sizeof(ptr));
+	if (ptr == NULL)
+	{
+		exit(98);
+	} else 
+	{
+		for (i = 0; i < n; i++)
+		{
+			ptr[i] = i + 1;
+		}
+	}
+	return (ptr);
+}
