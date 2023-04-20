b6f59630ad9c983bc8c2b389061a24772742884b
myusername <myemail@gmail.com>
2023-04-19 18:13:17 -0700

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,12 @@
 #include <stdlib.h>
+#include "function_pointers.h"
 
 /**
  * print_name - prints a name
  * @name: parameter
  * @f:function
  */
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (name != NULL && f != NULL)
