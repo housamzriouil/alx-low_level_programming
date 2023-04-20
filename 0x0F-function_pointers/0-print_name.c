4b57600f41576cfb2a411bb5b7574eaa9c729f30
myusername <myemail@gmail.com>
2023-04-19 18:03:53 -0700

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: parameter
+ * @f:function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,6 @@
+#ifndef _function_pointers.h_
+#define _function_pointers.h_
+
+void print_name(char *name, void (*f)(char *));
+
+#endif
