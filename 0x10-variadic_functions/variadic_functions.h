771005f84600ee8a2afceb1d7af3d64cca171e62
myusername <myemail@gmail.com>
2023-04-20 17:38:39 -0700

    print all

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,17 +6,6 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
-/**
- * struct dt - struct
- * @letter: letter
- * @func: function ptr
- */
-
-typedef struct dt
-{
-	char letter;
-	void (*func)(va_list);
-} datatype;
 
 
 #endif
